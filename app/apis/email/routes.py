458a53b6cca5070367fdc3cdfeab6384bde6bbee
Richard Khonan <richardkhonan0797@gmail.com>
2022-03-12 23:11:37 +0700

    first commit

---
+++ app/apis/email/routes.py
@@ -0,0 +1,8 @@
+from .resources.gui import Gui
+from .resources.save_email import SaveEmail
+from .resources.save_recipient import SaveRecipient
+
+def initialize_routes(api):
+    api.add_resource(SaveEmail, "/save_emails")
+    api.add_resource(SaveRecipient, "/save_recipient")
+    api.add_resource(Gui, "/")
