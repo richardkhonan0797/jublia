196a9998ab679793525c6f2e4faaefe0ae077a53
Richard Khonan <richardkhonan0797@gmail.com>
2022-03-13 20:24:24 +0700

    add schema EmailContentSchema and manage_email api

--- app/apis/email/resources/manage_email.py
+++ app/apis/email/resources/manage_email.py
@@ -12,16 +12,40 @@ from flask_restful import Resource
 from flask_sieve import Validator
 
 # Helpers import
-from app.helpers.send_email import send_email
 from app.helpers.redis import delete_multiple
 
 # Models import
 from app.models.email_content_model import EmailContent
-from app.models.email_recipient_model import EmailRecipient
 from app.models.email_recipient_content_model import EmailRecipientContent
+
+# Schemas import
+from app.schemas.email_content_schema import EmailContentSchema
 
 
 class ManageEmail(Resource):
+    def __init__(self):
+        self.email_content_schema = EmailContentSchema(many=True)
+
+    def get(self):
+        try:
+            email_contents = EmailContent.query.all()
+
+            response = {
+                "status": "success",
+                "status_code": 200,
+                "message": "success",
+                "data": self.email_content_schema.dump(email_contents)
+            }
+            return make_response(jsonify(response), 200)
+        except Exception as e:
+            current_app.logger.error(traceback.format_exc())
+            response = {
+                "status": "error",
+                "status_code": 500,
+                "message": "Internal Error",
+                "data": {},
+            }
+            return make_response(jsonify(response), 500)
 
     def delete(self):
         try:
@@ -42,7 +66,7 @@ class ManageEmail(Resource):
                 email_content_id = int(request.args.get("email_content_id"))
                 email_recipient_content = EmailRecipientContent.query.filter_by(email_content_id=email_content_id).all()
 
-                job_ids = ["rq:job:"+erc.job_id for erc in email_recipient_content]
+                job_ids = [erc.job_id for erc in email_recipient_content]
                 if job_ids:
                     delete_multiple(job_ids)
 
